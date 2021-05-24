102b2465fb6cb90fb3a9bd48b1152d9ce63737b5
Robert Israelsson <israelsson.r@gmail.com>
2021-05-24 21:38:55 +0200

    Added .htaccess and basic index page with controller.

---
+++ src/Controller/TodoController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
+class TodoController extends AbstractController
+{
+    /**
+     *
+     * @Route("/", name="index")
+     */
+    public function index(): Response
+    {
+        return $this->render('content/todo.html.twig', [
+            'title' => 'Todo List',
+        ]);
+    }
+}
